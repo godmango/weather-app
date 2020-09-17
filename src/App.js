d636c12289ce38ec56a5f6c1b09af80d03d2c847
Jun Young Cho <godguman@gmail.com>
2020-09-17 09:26:01 +0700

    weather done

--- src/App.js
+++ src/App.js
@@ -1,28 +1,77 @@
 import React, { Component } from "react";
-
+import "./App.css";
 export default class App extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			weather: null,
+			wData: null,
 		};
 	}
-
-	getWeather = async () => {
+	getLocation = () => {
+		navigator.geolocation.getCurrentPosition((post) => {
+			this.getWeather(post.coords.longitude, post.coords.latitude);
+		});
+	};
+	getCityWeather = async (selectedCity) => {
 		let apikey = process.env.REACT_APP_APIKEY;
-		console.log(apikey);
-		let url = `https://api.openweathermap.org/data/2.5/weather?q=saigon&appid=${apikey}&units=metric`;
+		let url = `https://api.openweathermap.org/data/2.5/weather?q=${selectedCity}&appid=${apikey}`;
+		let response = await fetch(url);
+		let data = await response.json();
+
+		this.setState({ wData: data });
+	};
+	componentDidMount() {
+		this.getLocation();
+	}
+	getWeather = async (longitude, latitude) => {
+		let apikey = process.env.REACT_APP_APIKEY;
+
+		let url;
+		if (latitude) {
+			url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apikey}`;
+		} else {
+			url = `https://api.openweathermap.org/data/2.5/weather?q=saigon&appid=${apikey}`;
+		}
+		console.log(url);
 		let response = await fetch(url);
 		let data = await response.json();
 		console.log("data", data);
-		this.setState({ weather: data });
+		this.setState({ wData: data });
 	};
 	componentDidMount() {
-		this.getWeather();
+		this.getLocation();
 	}
 
 	render() {
-		return <div>{this.state.weather && this.state.weather.main.temp}</div>;
+		return (
+			<div className="theWholeThing">
+				{" "}
+				<div className="weatherDisplay">
+					<p>City: {this.state.wData && this.state.wData.name}</p>
+					<p>
+						Temperature:{" "}
+						{this.state.wData &&
+							Math.round(this.state.wData.main.temp - 273.15)}
+						°C /{" "}
+						{this.state.wData &&
+							Math.round(((this.state.wData.main.temp - 273.15) * 9) / 5 + 32)}
+						°F
+					</p>
+					<p>Humidity: {this.state.wData && this.state.wData.main.humidity}%</p>
+					<p>{this.state.wData && this.state.wData.weather[0].description}</p>
+				</div>
+				<div className="cityButton">
+					<button onClick={() => this.getCityWeather("Голубинка")}>
+						Голубинка
+					</button>
+					<button onClick={() => this.getCityWeather("seoul")}>Seoul</button>
+					<button onClick={() => this.getCityWeather("tokyo")}>Tokyo</button>
+					<button onClick={() => this.getCityWeather("New York City")}>
+						New York City
+					</button>
+				</div>
+			</div>
+		);
 	}
 }
